372d659a43c29092ef8e91692aa44a6f498312ef
rrossilli55 <rrossilli55@gmail.com>
2024-02-20 16:00:18 -0600

    quick fix

--- apps/landing/components/containers/AppContainer.tsx
+++ apps/landing/components/containers/AppContainer.tsx
@@ -1,22 +1,7 @@
 'use client'
-import WagmiProvider from '@/components/providers/WagmiProvider'
-import RainbowProvider from '../providers/RainbowProvider'
-// import { PostHogProvider } from 'posthog-js/react'
 import { ReactNode } from 'react'
-import { MusicProvider } from '../providers/MusicProvider'
 import { PHProvider } from '@/hooks/PostHogProvider'
-import { DarkModeProvider } from '../providers/DarkModeProvider'
 
 export default function AppContainer({ children }: { children: ReactNode }) {
-    return (
-        <WagmiProvider>
-            <RainbowProvider>
-                <MusicProvider>
-                    <DarkModeProvider>
-                        <PHProvider>{children}</PHProvider>
-                    </DarkModeProvider>
-                </MusicProvider>
-            </RainbowProvider>
-        </WagmiProvider>
-    )
+    return <PHProvider>{children}</PHProvider>
 }
