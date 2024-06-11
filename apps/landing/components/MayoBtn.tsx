4bffae08b28d6427b0e6fd6a75e068c9962c2c80
Ufort Stephen <ufortstephen@gmail.com>
2024-06-11 19:48:00 +0100

    fix: mayo hero

--- apps/landing/components/MayoBtn.tsx
+++ apps/landing/components/MayoBtn.tsx
@@ -14,9 +14,9 @@ export default function MayoBtn() {
                 //     progress: undefined,
                 // })
             }}
-            className="relative w-full h-[109px] flex items-center"
+            className="relative w-full h-[109px] flex items-center "
         >
-            <div className="w-full cursor-pointer z-10 align-middle px-[24px] py-[12px] md:text-center justify-start items-center h-[50px] rounded-[60px] bg-[linear-gradient(0deg,#8000E4_0%,#962DFE_100%)] flex gap-[10px]">
+            <div className="w-full cursor-pointer z-10 align-middle px-[24px] py-[12px] md:text-center justify-center items-center h-[50px] rounded-[60px] bg-[linear-gradient(0deg,#8000E4_0%,#962DFE_100%)] flex gap-[10px]">
                 <div className="hidden md:flex text-[16px] font-[400] text-white">
                     {CONTRACT_ADDRESS}
                 </div>
